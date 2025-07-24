38a721a6b27a8a6aeee2c3c346e5ba81060f88c5
e542137 <kevin.werner@sbb.ch>
2025-07-24 17:32:29 +0200

    Add RAG functionality
    - create embeddings with Ollama embedding model
    - add QuestionAnswerAdvisor to ChatClient

---
+++ src/main/java/ch/akros/workshop/ragfaq/MarkdownLoader.java
@@ -0,0 +1,54 @@
+package ch.akros.workshop.ragfaq;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import org.springframework.ai.document.Document;
+
+public final class MarkdownLoader {
+
+    private MarkdownLoader() {
+    }
+
+    static List<Document> loadMarkdownFiles(String docsPath) {
+        var docsUrl = MarkdownLoader.class.getClassLoader().getResource(docsPath);
+        assert docsUrl != null : "Documentation path not found: " + docsPath;
+        try (var paths = Files.walk(Paths.get(docsUrl.getPath()))) {
+            return paths
+                .filter(Files::isRegularFile)
+                .map(path -> {
+                    try {
+                        return new Document(Files.readString(path));
+                    } catch (IOException e) {
+                        throw new IllegalStateException(String.format("Could not read file %s!", path.getFileName()), e);
+                    }
+                })
+                .toList();
+        } catch (Exception e) {
+            throw new IllegalStateException("Something went wrong during reading Markdown files!", e);
+        }
+    }
+
+    static List<Document> loadMarkdownFilesByLine(String docsPath) {
+        var docsUrl = MarkdownLoader.class.getClassLoader().getResource(docsPath);
+        assert docsUrl != null : "Documentation path not found: " + docsPath;
+        try (var paths = Files.walk(Paths.get(docsUrl.toURI()))) {
+            return paths
+                .filter(Files::isRegularFile)
+                .map(path -> {
+                    try {
+                        return Files.readAllLines(path).stream()
+                            .map(Document::new)
+                            .toList();
+                    } catch (IOException e) {
+                        throw new IllegalStateException(String.format("Could not read file %s!", path.getFileName()), e);
+                    }
+                })
+                .flatMap(List::stream)
+                .toList();
+        } catch (Exception e) {
+            throw new IllegalStateException("Something went wrong during reading Markdown files!", e);
+        }
+    }
+}
--- src/main/java/ch/akros/workshop/ragfaq/RagfaqApplication.java
+++ src/main/java/ch/akros/workshop/ragfaq/RagfaqApplication.java
@@ -1,50 +1,79 @@
 package ch.akros.workshop.ragfaq;
 
+import java.util.List;
 import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 @SpringBootApplication
 public class RagfaqApplication {
 
-	@Bean
-	ChatClient ai(PromptChatMemoryAdvisor promptChatMemoryAdvisor,
-		ChatClient.Builder aibuilder) {
-		var system = """
+    private static final Logger LOG = LoggerFactory.getLogger(RagfaqApplication.class);
+
+    @Bean
+    ChatClient ai(PromptChatMemoryAdvisor promptChatMemoryAdvisor, VectorStore vectorStore,
+        ChatClient.Builder aibuilder) {
+        var system = """
             Du bist ein KI-gestützter Assistent, der Menschen hilft, die AcmeCloud verwenden wollen. Nachfolgend findest du die relevanten Teile der AcmeCloud 
             Dokumentation. Wenn du dort keine Informationen findest, dann antworte höflich, dass du leider nicht weiterhelfen kannst und sie könnten den Support
             unter +41 31 123 45 67 anrufen.""";
 
-		return aibuilder
-			.defaultSystem(system)
-			.defaultAdvisors(promptChatMemoryAdvisor)
-			.build();
-	}
+        return aibuilder
+            .defaultSystem(system)
+            .defaultAdvisors(promptChatMemoryAdvisor, new QuestionAnswerAdvisor(vectorStore))
+            .build();
+    }
 
-	@Bean
-	PromptChatMemoryAdvisor promptChatMemoryAdvisor(DataSource dataSource) {
-		var jdbc = JdbcChatMemoryRepository
-			.builder()
-			.dataSource(dataSource)
-			.build();
+    @Bean
+    PromptChatMemoryAdvisor promptChatMemoryAdvisor(DataSource dataSource) {
+        var jdbc = JdbcChatMemoryRepository
+            .builder()
+            .dataSource(dataSource)
+            .build();
 
-		var chatMessageWindow = MessageWindowChatMemory
-			.builder()
-			.chatMemoryRepository(jdbc)
-			.build();
+        var chatMessageWindow = MessageWindowChatMemory
+            .builder()
+            .chatMemoryRepository(jdbc)
+            .build();
 
-		return PromptChatMemoryAdvisor
-			.builder(chatMessageWindow)
-			.build();
-	}
+        return PromptChatMemoryAdvisor
+            .builder(chatMessageWindow)
+            .build();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(RagfaqApplication.class, args);
-	}
+    @Bean
+    CommandLineRunner initializeVectorStore(JdbcClient db, VectorStore vectorStore) {
+        return args -> {
+            var count = db
+                .sql("select count(*) from vector_store")
+                .query(Integer.class)
+                .single();
+            if (count == 0) {
+                LOG.info("No vectors found, initializing vector store...");
+                List<Document> documents = MarkdownLoader.loadMarkdownFiles("docs");
+                vectorStore.add(documents);
+                LOG.info("Successfully initialized vector store with {} vectors.", documents.size());
+
+            } else {
+                LOG.info("Vector store already initialized with {} vectors.", count);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagfaqApplication.class, args);
+    }
 
 }
